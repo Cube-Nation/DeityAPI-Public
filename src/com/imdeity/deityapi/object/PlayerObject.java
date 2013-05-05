511957606035b059dd579e2487c4c524500f325f
DerFlash <bte@freenet.de>
2013-05-05 11:37:11 +0200

    Updated to 1.5.2-R0.1 (beta)

--- src/com/imdeity/deityapi/object/PlayerObject.java
+++ src/com/imdeity/deityapi/object/PlayerObject.java
@@ -2,13 +2,13 @@ package com.imdeity.deityapi.object;
 
 import java.util.ArrayList;
 
-import net.minecraft.server.v1_5_R2.Packet39AttachEntity;
+import net.minecraft.server.v1_5_R3.Packet39AttachEntity;
 
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_5_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_5_R3.entity.CraftPlayer;
 import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
